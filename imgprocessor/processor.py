5bacd7e317cb23239d6624afcbebaf5ae15b369e
hu.leping <hu.leping@hellogroup.com>
2026-03-19 17:31:38 +0800

    docs: fix

--- imgprocessor/processor.py
+++ imgprocessor/processor.py
@@ -33,8 +33,10 @@ class ProcessorCtr(object):
 
         if fmt:
             if fmt.upper() == enums.ImageFormat.JPEG.value and im.mode not in ["RGB", "L"]:
+                # jpeg 一般支持 RGB、L
                 im = im.convert("RGB")
             elif fmt.upper() == enums.ImageFormat.WEBP.value and im.mode == "P" and has_transparency(im):
+                # webp 透明场景通常使用RGBA(首选,全彩) 、LA(黑白); P模式可能会丢失info中的透明信息
                 im = im.convert("RGBA")
 
         if not kwargs.get("quality"):
